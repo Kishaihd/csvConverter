f7698039c6c3ebf4e72f627d3fe3f06467d9b26f
Kishaihd <mr.pinstripe_Suit@hotmail.com>
2015-08-30 22:45:50 -0600

    First commit.
    Variables declared.

---
+++ bin/main.dart
@@ -0,0 +1,39 @@
+
+
+void main() {
+  // Order of data.
+  String name;
+  String sizeAndType;
+  String alignment;
+  String armorClass;
+  String hitPoints;
+  String hitDice;
+  String speedType1;
+  String speedType2;
+  String speedType3;
+  List<String> speeds; // This will cover any existing speeds.
+
+  String strength;
+  String dexterity;
+  String constitution;
+  String intelligence;
+  String wisdom;
+  String charisma;
+
+  List<String> savingThrows;
+  List<String> skills;
+  List<String> damageResistances;
+  List<String> damageImmunities;
+  List<String> conditionImmunities;
+  List<String> senses; //Darkvision, blindsense, passive perception included here!
+  List<String> languages;
+  String challengeRating;
+  String experience;
+  // Empty entry! // Can't find anything that uses this? They're all blank!
+  List<String> actions; // eg Club. Melee Weapon Attack: +2 to hit, reach 5 ft., one target.<br>Hit: 2 (1d4) bludgeoning damage.
+  List<String> legendaryActions;
+  List<String> features;
+
+
+}
+
