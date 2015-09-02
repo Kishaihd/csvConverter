53cbc35f14f487a446764b3e3d36d88f143d0386
Kishaihd <mr.pinstripe_Suit@hotmail.com>
2015-09-01 19:42:12 -0600

    writing blank files...?
    Why?!

--- bin/main.dart
+++ bin/main.dart
@@ -8,48 +8,68 @@ File outFile;
 String delimiter;
 
 Stream<List<int>> inputIntStream;
+Stream  StringStream;
 List<String> rawStringList = [];
-
-List<Map<String, String>> finalMap = [];
+StringBuffer toWriteToFile;
+//List<Map<String, String>> finalMap = [];
 
 void main() {
-  getIncomingFilePath();
+  toWriteToFile = new StringBuffer();
   delimiter = ";";
   // todo expand this to eventually take class spell lists. User will choose.
 
+  getIncomingFilePath();
+  readFile(incomingFile);
+  StringStream
+    .transform(new LineSplitter())
+    .listen((String line) {
+      writeToFile(line.split(delimiter));
+    });
 
-  // Order of data.
-  String name;
-  String sizeAndType;
-  String alignment;
-  String armorClass;
-  String hitPoints;
-  String hitDice;
-  String speedType1;
-  String speedType2;
-  String speedType3;
-  List<String> speeds; // This will cover any existing speeds.
+  stdout.write("Enter a name for the new file: ");
+  String outFile = stdin.readLineSync();
+  outFile += ".txt";
+  print('$toWriteToFile');
+  print('${toWriteToFile.toString()}');
+  var myFile = new File('C:/Users/HAQ/Google Drive/DnD/${outFile}');
+  myFile.writeAsString(toWriteToFile.toString())
+  .then((_) => stdout.writeln("\n$outFile successfully created!") );
 
-  String strength;
-  String dexterity;
-  String constitution;
-  String intelligence;
-  String wisdom;
-  String charisma;
 
-  List<String> savingThrows;
-  List<String> skills;
-  List<String> damageResistances;
-  List<String> damageImmunities;
-  List<String> conditionImmunities;
-  List<String> senses; //Darkvision, blindsense, passive perception included here!
-  List<String> languages;
-  String challengeRating;
-  String experience;
-  // Empty entry! // Can't find anything that uses this? They're all blank!
-  List<String> actions; // eg Club. Melee Weapon Attack: +2 to hit, reach 5 ft., one target.<br>Hit: 2 (1d4) bludgeoning damage.
-  List<String> legendaryActions;
-  List<String> features;
+  //writeToFile()
+//
+//  // Order of data.
+//  String name;
+//  String sizeAndType;
+//  String alignment;
+//  String armorClass;
+//  String hitPoints;
+//  String hitDice;
+//  String speedType1;
+//  String speedType2;
+//  String speedType3;
+//  List<String> speeds; // This will cover any existing speeds.
+//
+//  String strength;
+//  String dexterity;
+//  String constitution;
+//  String intelligence;
+//  String wisdom;
+//  String charisma;
+//
+//  List<String> savingThrows;
+//  List<String> skills;
+//  List<String> damageResistances;
+//  List<String> damageImmunities;
+//  List<String> conditionImmunities;
+//  List<String> senses; //Darkvision, blindsense, passive perception included here!
+//  List<String> languages;
+//  String challengeRating;
+//  String experience;
+//  // Empty entry! // Can't find anything that uses this? They're all blank!
+//  List<String> actions; // eg Club. Melee Weapon Attack: +2 to hit, reach 5 ft., one target.<br>Hit: 2 (1d4) bludgeoning damage.
+//  List<String> legendaryActions;
+//  List<String> features;
 
 }
 
@@ -64,11 +84,11 @@ String getIncomingFilePath() {
     try {
       incomingFile = new File(csvPath);
       done = true;
-      readFile(incomingFile);
+      //readFile(incomingFile);
       return "continue";
     } catch(e) {
       stdout.writeln("Error: file not found or path not valid.\nTry new file? (y/n) : ");
-      String tryAgainAnswer = stdin.readLineSync().toLowerCase();
+      String tryAgainAnswer = stdin.readLineSync();
       if (tryAgainAnswer == 'y') {
         done = false;
       }
@@ -78,124 +98,109 @@ String getIncomingFilePath() {
       }
     } // end on StateError
   } // end while(!done)
-
+  return "done";
 } // end getIncomingFilePath()
 
 void readFile(File file) {
   inputIntStream = file.openRead();
-  inputIntStream
-    .transform(UTF8.decoder)
-    .transform(new LineSplitter())
-    .listen((String line) {
-    parseCVS(line);
-  });
-
+  print("InputIntStream: ${inputIntStream.toString()}");
+  StringStream = inputIntStream.transform(UTF8.decoder);
+  print('StringStream is:\n${StringStream}');
+//    .transform(new LineSplitter());
+//    .listen((String line) {
+//    parseCVS(line);
+//  });
 }
 
-void parseCVS(String cvsString) {
-  List<String> singleEntry = [];
-
-  singleEntry = cvsString.split(delimiter);
-  writeToFile(singleEntry);
-}
+//void parseCVS(String cvsString) {
+//  List<String> singleEntry = [];
+//
+//  singleEntry = cvsString.split(delimiter);
+//  writeToFile(singleEntry);
+//}
 
 void writeToFile(List<String> li) {
-  StringBuffer toWriteToFile = new StringBuffer();
+  print("List to be written to file: $li");
 
-  print(li.length);
-  print(li.toString());
-//  cvsMap["name"] = li[0];
-//  cvsMap["sizeAndType"] = li[1];
-//  cvsMap["alignment"] = li[2];
-//  cvsMap["armorClass"] = li[3];
-//  cvsMap["hitPoints"] = li[4];
-//  cvsMap["hitDice"] = li[5];
-//  cvsMap["speeds"] = li[6]; // Should this just be a string?
-//  cvsMap["strength"] = li[7];
-//  cvsMap["dexterity"] = li[8];
-//  cvsMap["constitution"] = li[9];
-//  cvsMap["intelligence"] = li[10];
-//  cvsMap["wisdom"] = li[11];
-//  cvsMap["charisma"] = li[12];
-//  cvsMap["savingThrows"] =li[13];
-//  cvsMap["skills"] = li[14];
-//  cvsMap["damageResistance"] = li[15];
-//  cvsMap["damageImmunity"] = li[16];
-//  cvsMap["conditionImmunity"] = li[17];
-//  cvsMap["senses"] = li[18];
-//  cvsMap["languages"] = li[19];
-//  cvsMap["challengeRating"] = li[20];
-//  cvsMap["experience"] = li[21];
-//  cvsMap["empty"] = li[22];
-//  cvsMap["actions"] = li[23];
-//  cvsMap["legendaryAction"] = li[24];
-//  cvsMap["features"] = li[25];
+  toWriteToFile.writeln("""
+    {
+      "name": "${li[0]}",
+      "sizeAndType": "${li[1]}",
+      "alignment": "${li[2]}",
+      "armorClass": "${li[3]}",
+      "hitPoints": "${li[4]}",
+      "hitDice": "${li[5]}",
+      "speeds": "${li[6]}",
+      "strength": "${li[7]}",
+      "dexterity": "${li[8]}",
+      "constitution": "${li[9]}",
+      "intelligence": "${li[10]}",
+      "wisdom": "${li[11]}",
+      "charisma": "${li[12]}",
+      "savingThrows": "${li[13]}",
+      "skills" : "${li[14]}",
+      "damageResistance" : "${li[15]}",
+      "damageImmunity" : "${li[16]}",
+      "conditionImmunity" : "${li[17]}",
+      "senses" : "${li[18]}",
+      "languages" : "${li[19]}",
+      "challengeRating" : "${li[20]}",
+      "experience" : "${li[21]}",
+      "empty" : "${li[22]}",
+      "actions" : "${li[23]}",
+      "legendaryAction" : "${li[24]}",
+      "features" : "${li[25]}"
+    }
+    """);
+//  if (i != (li.length - 1)) {
+//    toWriteToFile.write(",\n");
+//  }
 
-//  cvsMap["name"] = li[0];
-//  cvsMap["sizeAndType"] = li[1];
-//  cvsMap["alignment"] = li[2];
-//  cvsMap["armorClass"] = li[3];
-//  cvsMap["hitPoints"] = li[4];
-//  cvsMap["hitDice"] = li[5];
+//  print(li.length);
+  //print(li.toString());
+//  int idx = -1;
+//  for (int i = 0; i < li.length; i++ ) {
 //
-//  cvsMap["speeds"] = new List.from(li[6].split(","));
-//  cvsMap["speeds"] = li[6]; // Should this just be a string?
-//  cvsMap["strength"] = li[7];
-//  cvsMap["dexterity"] = li[8];
-//  cvsMap["constitution"] = li[9];
-//  cvsMap["intelligence"] = li[10];
-//  cvsMap["wisdom"] = li[11];
-//  cvsMap["charisma"] = li[12];
-//  cvsMap["savingThrows"] = new List.from(li[13].split(","));
-//  cvsMap["skills"] = new List.from(li[14].split(","));
-//  cvsMap["damageResistance"] = new List.from(li[15].split(","));
-//  cvsMap["damageImmunity"] = new List.from(li[16].split(","));
-//  cvsMap["conditionImmunity"] = new List.from(li[17].split(","));
-//  cvsMap["senses"] = new List.from(li[18].split(","));
-//  cvsMap["languages"] = new List.from(li[19].split(","));
-//  cvsMap["challengeRating"] = li[20];
-//  cvsMap["experience"] = li[21];
-//  cvsMap["empty"] = li[22];
-//  cvsMap["actions"] = new List.from(li[23].split(","));
-//  cvsMap["legendaryAction"] = new List.from(li[24].split(","));
-//  cvsMap["features"] = new List.from(li[25].split(","));
+//    toWriteToFile.write("""
+//    {
+//      "name": "${li[++idx]}",
+//      "sizeAndType": "${li[++idx]}",
+//      "alignment": "${li[++idx]}",
+//      "armorClass": "${li[++idx]}",
+//      "hitPoints": "${li[++idx]}",
+//      "hitDice": "${li[++idx]}",
+//      "speeds": "${li[++idx]}", // Should this just be a string?
+//      "strength": "${li[++idx]}",
+//      "dexterity": "${li[++idx]}",
+//      "constitution": "${li[++idx]}",
+//      "intelligence": "${li[++idx]}",
+//      "wisdom": "${li[++idx]}",
+//      "charisma": "${li[++idx]}",
+//      "savingThrows": "${li[++idx]}",
+//      "skills" : "${li[++idx]}",
+//      "damageResistance" : "${li[++idx]}",
+//      "damageImmunity" : "${li[++idx]}",
+//      "conditionImmunity" : "${li[++idx]}",
+//      "senses" : "${li[++idx]}",
+//      "languages" : "${li[++idx]}",
+//      "challengeRating" : "${li[++idx]}",
+//      "experience" : "${li[++idx]}",
+//      "empty" : "${li[++idx]}",
+//      "actions" : "${li[++idx]}",
+//      "legendaryAction" : "${li[++idx]}",
+//      "features" : "${li[++idx]}"
+//    }
+//    """);
+//      if (i != (li.length - 1)) {
+//        toWriteToFile.write(",\n");
+//      }
+//  }
 
-  toWriteToFile.write("""
-  {
-    "name": "${li[0]}",
-    "sizeAndType": "${li[1]}",
-    "alignment": "${li[2]}",
-    "armorClass": "${li[3]}",
-    "hitPoints": "${li[4]}",
-    "hitDice": "${li[5]}",
-    "speeds": "${li[6]}", // Should this just be a string?
-    "strength": "${li[7]}",
-    "dexterity": "${li[8]}",
-    "constitution": "${li[9]}",
-    "intelligence": "${li[10]}",
-    "wisdom": "${li[11]}",
-    "charisma": "${li[12]}",
-    "savingThrows": "${li[13]}",
-    "skills" : "${li[14]}",
-    "damageResistance" : "${li[15]}",
-    "damageImmunity" : "${li[16]}",
-    "conditionImmunity" : "${li[17]}",
-    "senses" : "${li[18]}",
-    "languages" : "${li[19]}",
-    "challengeRating" : "${li[20]}",
-    "experience" : "${li[21]}",
-    "empty" : "${li[22]}",
-    "actions" : "${li[23]}",
-    "legendaryAction" : "${li[24]}",
-    "features" : "${li[25]}"
-  }
-  """);
-
-  stdout.writeln("Enter a name for the new file: ");
-  String outFile = stdin.readLineSync();
-
-  new File('C:/Users/HAQ/Google Drive/DnD/${outFile}').writeAsString(toWriteToFile.toString())
-      .then((_) => stdout.writeln("$outFile successfully created!") );
+//  stdout.writeln("Enter a name for the new file: ");
+//  String outFile = stdin.readLineSync();
+//  outFile += ".txt";
+//  new File('C:/Users/HAQ/Google Drive/DnD/${outFile}').writeAsString(toWriteToFile.toString())
+//.then((_) => stdout.writeln("$outFile successfully created!") );
 
 }
 
